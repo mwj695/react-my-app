952fa635907e53cae2926042b4314a0da25a5bd1
maor <2927421075@qq.com>
2021-08-26 14:06:06 +0800

    001

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
 
 function App() {
+  const [count,setCount]=useState{0}
   return (
     <div className="App">
       123123
+      <p>clicked {count} times</p>
+      <button onclick={()=>setCount(count+1)}>click here</button>
+
     </div>
   );
 }
